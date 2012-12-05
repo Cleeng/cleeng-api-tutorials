b581de704035a83969de0311eb60529469c86748
tomekszad <tomek.szad@gmail.com>
2012-12-05 16:44:24 +0100

    Manage your offers

---
+++ 04_Manage_your_offers/purchase.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Cleeng API Example 3 - Cleeng Sandbox
+ * you can find tutorial 3 here: http://open.cleeng.com/Tutorials/02_Cleeng_Sandbox
+ * =====================================================================
+ * This file should be opened in browser, probably:
+ * your-site.com/03_Cleeng_Sandbox/purchase.php
+ */
+
+
+// set the ID of the offer you want to sell
+$offerId = 'YOUR_OFFER_ID'; // R733029903_US - default offerID for sandbox
+
+
+// include PHP SDK
+include_once('../cleeng-php-sdk/cleeng_api.php');
+
+$cleengApi = new Cleeng_Api();
+
+//set endpoint to Sandbox
+$cleengApi->setEndpoint('https://sandbox.cleeng.com/api/3.0/json-rpc');
+
+?>
+<html>
+<head>
+    <script type="text/javascript" src="http://sandbox.cleeng.com/js-api/3.0/api.js"></script>
+    <script type="text/javascript">
+        CleengApi.trackOfferImpression("<?php echo $offerId ?>");
+        function cleengPurchase() {
+            CleengApi.purchase("<?php echo $offerId ?>", function (result) {
+                window.location.reload();
+            });
+        }
+    </script>
+</head>
+<body>
+<h1>Example 1: Getting started with Cleeng</h1>
+<p>In case of any problems, please go back to <a href="http://cleeng.com/open/Tutorials/03_Cleeng_Sandbox">tutorial</a> on Cleeng Open.</p>
+
+<hr />
+
+<?php
+
+// Check if visitor has access to protected content
+if ($cleengApi->isAccessGranted($offerId)): ?>
+
+Yeah!! You have access to offer <?php echo $offerId ?>. You can define any html here that you want to sell. You have now access to this anywhere, anytime.
+<br /><br />
+Please note:
+<ul>
+    <li>A secure token is set, so next time you go to this page (using this browser) you have instant access again. </li>
+    <li>To access this content from another device, just log in to Cleeng and visit this page. The content is automatically revealed.</li>
+    <li>To have an overview of all items bought, check your <a href="http://sandbox.cleeng.com/my-account/">Sandbox Cleeng library</a>. Now this item is listed there as well.</li>
+    <li>Be aware that as a publisher you have always instant access to the content you published.</li>
+</ul>
+
+    <?php else: ?>
+
+Content is not accessible for you. <br /><br />
+<form>
+    <input type="button" onClick="javascript:cleengPurchase();" value="Buy access to this content" />
+</form>
+
+    <?php endif; ?>
+
+<hr />
+<p>
+    &copy; Safe & secure content protection and monetization by
+    <a href="http://cleeng.com" title="Cleeng Content Monetization" target="_blank">
+        <img src="http://cdn.cleeng.com/images/layout/cleeng_logo_small.png" alt="Cleeng Content Monetization" title="Cleeng Content Monetization" />
+    </a>.
+</p>
+
+</body>
+</html>
