6b080228f73e7fbdfeabc83e6420622f93011903
tomekszad <tomek.szad@gmail.com>
2012-12-05 18:27:34 +0100

    manage your content examples

---
+++ 04_Manage_your_offers/deactivate_offer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Cleeng API Example 4 - Creating offer in Sandbox
+ */
+
+/***
+ * PUBLISHER TOKEN
+ * get your token from http://sandbox.cleeng.com/dev/api-keys
+ */
+$publisherToken = 'TeurE3xRXcFtF7gSbhYCXx_qC1IrvnDWGejDv114-OE0cdZ9';
+
+
+$offerId = R495315693_PL;
+
+$offerSetup = array(
+    'title' => 'Super Cool article for just $0.49 with longer access!',
+    'period' => '72',
+    'description' => 'This is my first Rental Offer, after buying this, you will get 72 hours of accesss to my Super Cool article.'
+);
+
+include_once('../cleeng-php-sdk/cleeng_api.php');
+
+$cleengApi = new Cleeng_Api(array(
+    'endpoint' => 'https://sandbox.cleeng.com/api/3.0/json-rpc',
+    'publisherToken' => $publisherToken
+));
+
+try {
+
+$offer = $cleengApi->updateRentalOffer($offerId, $offerSetup);
+
+echo 'You\'ve just updated offer with id = ' . $offer->id . "<br/><br/>";
+?>
+
+Some params:<br/>
+
+ID - <?php echo $offer->id ?><br/>
+Title - <?php echo $offer->title ?><br/>
+Period - <?php echo $offer->period ?><br/>
+
+<?php
+}
+catch(Exception $e) {
+    echo $e->getMessage();
+}
+?>
+
+<hr/>
+Read more in updateRentalOffer Reference!
+
+<hr />
+<p>&copy; Safe & secure content protection and monetization by <a href="http://cleeng.com" title="Cleeng Content Monetization" target="_blank"><img src="http://cdn.cleeng.com/images/layout/cleeng_logo_small.png" alt="Cleeng Content Monetization" title="Cleeng Content Monetization" /></a>.</p>
